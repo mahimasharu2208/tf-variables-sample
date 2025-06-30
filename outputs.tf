695605334d49a84e86dca77c675a4598daa05d63
Mahima Sharu <mahima.sharu@hashicorp.com>
2025-06-30 13:14:13 +0530

    variable

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "name_output" {
+  description = "This outputs the value of the 'name' variable"
+  value       = var.name
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "name" {
+  description = "A sample name"
+  type        = string
+  default     = "TerraformUser"
+}
